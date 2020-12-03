ec94e4154025bae786d9ae3df70bd716b639024f
zhangyaqi <992321066@qq.com>
2020-12-03 10:32:50 +0800

    tijiao

--- mall-springboot/src/main/java/com/edu/springboot/App.java
+++ mall-springboot/src/main/java/com/edu/springboot/App.java
@@ -10,7 +10,7 @@ import org.springframework.context.ConfigurableApplicationContext;
  */
 @SpringBootApplication
 public class App 
-{
+{ 
     public static void main( String[] args ){
         ConfigurableApplicationContext context = SpringApplication.run(App.class, args);
     }
